7a1f453e63e748de60cbdaadc3d7e8c8aecee193
Balaji Konduru <kondurubalaji1234@gmail.com>
2025-07-15 03:14:02 +0530

    Minor fix:just added catch in problemController in createStatement function

--- controllers/problemController.js
+++ controllers/problemController.js
@@ -11,11 +11,17 @@ exports.createStatement=async(req,res)=>{
         return res.json({success:false,message:"the above fileds are mossing"})
 
     }
+    try {
+        
+        const problem=new problemModel({hackathonName,teamName,membersRequired,registrationDeadline,skillsRequired,description,createdBy:userID})
+        await problem.save()
+    
+        return res.json({success:true,message:"Problem is stored successfully"})
+    } catch (error) {
+        return res.json({success:true,message:error.message})
+        
+    }
 
-    const problem=new problemModel({hackathonName,teamName,membersRequired,registrationDeadline,skillsRequired,description,createdBy:userID})
-    await problem.save()
-
-    return res.json({success:true,message:"Problem is stored successfully"})
 
 
 
